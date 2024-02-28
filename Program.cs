697e200396d6c34023f0a81b2548e211f946fddb
belehx <161518280+belehx@users.noreply.github.com>
2024-02-27 21:56:49 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exerc2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Digite um número: ");
+            a = int.Parse(Console.ReadLine());  
+
+            if (a >= 20 && a <= 100) 
+            {
+                Console.WriteLine(a);
+            } else
+            {
+                Console.WriteLine("O número não poderá ser exibido");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
